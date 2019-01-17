afe436681e623e06ca81c27541e53255ed22a236
chengtf94 <chengtf94@126.com>
2019-01-17 20:44:13 +0800

    modify

--- src/main/java/lambdasinaction/chap1/FilteringApples.java
+++ src/main/java/lambdasinaction/chap1/FilteringApples.java
@@ -96,6 +96,7 @@ public class FilteringApples{
             this.color = color;
         }
 
+        @Override
         public String toString() {
             return "Apple{" +
                    "color='" + color + '\'' +
